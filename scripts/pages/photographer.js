05d24cd0b268b39e1243ca2d23c7dc54c553dc63
NKSnT <bassinet.nahel@gmail.com>
2023-09-22 17:25:47 +0200

    done with the maine page, start of the detail page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,35 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const urlId = window.location.search;
+const urlSearchParams = new URLSearchParams(urlId);
+const photographerId = urlSearchParams.get('id');
+console.log(photographerId);
+
+async function getPhotographers() {
+    return await fetch('./data/photographers.json')
+        .then((res) => res.json())
+        .catch((err) => console.log('Unexpected error occured', err));
+}
+async function displayData(photographers) {
+    const photographersHeader = document.querySelector('.photograph-header');
+    const button = document.querySelector('.contact_button');
+    photographers.forEach((photographer) => {
+        if (photographer.id == photographerId) {
+            const photographerModel = photographerTemplate(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            const newDiv = document.createElement('div');
+            newDiv.appendChild(userCardDOM.firstChild.nextSibling);
+            newDiv.appendChild(userCardDOM.firstChild.nextSibling);
+            newDiv.appendChild(userCardDOM.firstChild.nextSibling);
+            userCardDOM.firstChild.insertAdjacentElement('afterend', button);
+            userCardDOM.appendChild(newDiv);
+            photographersHeader.appendChild(userCardDOM);
+        }
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,40 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    console.log(data);
+    const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    //const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/account.png`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        //const detail = document.createElement('a');
+        //detail.setAttribute('href', './photographer.html?id=' + id);
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        //img.setAttribute('src', picture);
+        img.setAttribute('src', picture);
+        img.setAttribute('alt', 'photo de ' + name);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.innerText = city + ', ' + country;
+        location.className = 'location';
+        const day_rate = document.createElement('p');
+        day_rate.innerText = price + '/jour';
+        day_rate.className = 'price';
+        const quote = document.createElement('p');
+        quote.innerText = tagline;
+        quote.className = 'tagline';
+
+        //detail.appendChild(img);
+        //detail.appendChild(h2);
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        //article.appendChild(detail);
+        article.appendChild(location);
+        article.appendChild(quote);
+        article.appendChild(day_rate);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
